65bbf528ffc0098e4873fe6eff4416da8fcf32db
Alexander <heinzosesrunge@gmail.com>
2019-10-26 17:08:12 -0300

    agrega microclima <- boton agregar

--- src/Components/Microclime/page.js
+++ src/Components/Microclime/page.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './styles.css';
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import { ImageGradient } from 'material-ui/svg-icons';
+
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -10,6 +12,11 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+
+//IMPORTS NECESARIOS PARA BOTONES
+import DeleteIcon from '@material-ui/icons/Delete';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
 const useStyles = makeStyles((theme) => {
     createStyles({
@@ -21,14 +28,13 @@ const useStyles = makeStyles((theme) => {
             height: 145,
             width: 80,
         },
-        root: {
-            flexGrow: 1,
-            height:'100%',
-            width:'100%',
-        },
         paper: {
             textAlign: 'center',
         },
+        fab: {
+		  margin: theme.spacing(1),
+		  float:'right',
+		}
     })
 });
 
@@ -38,11 +44,9 @@ export default function Page(props) {
     const classes = useStyles();
 
     return (
-        <div>
-            <br/>
-            {FormRow()}
-            <br/>
-        </div>
+		<div className="root">
+		    {FormRow()}
+		</div>
     );
 }
 
@@ -91,18 +95,32 @@ function FormRow() {
     const classes = useStyles();
 
     return (
-      <React.Fragment>
-        <Grid item xs={6} sm={6}>
-          <Paper className={classes.paper} >{RailCard("Test-1","Natural","Alta","Acelga",10000000,600,60000)}</Paper>
-        </Grid>
-        <br/>
-        <Grid item xs={6} sm={20}>
-          <Paper className={classes.paper} >{RailCard("Test-2","Natural","Alta","Acelga",10000000,600,60000)}</Paper>
-        </Grid>
-        <br/>
-        <Grid item xs={6}  sm={20}>
-          <Paper className={classes.paper}>{RailCard("Test-3","Natural","Alta","Acelga",10000000,600,60000)}</Paper>
-        </Grid>
-      </React.Fragment>
+    	<div>
+    		<div className="row">
+				<div className="col">
+					<Fab aria-label="add" className={classes.fab, "add"}>
+						<AddIcon />
+					</Fab>
+				</div>
+			</div>
+			<br/>
+			<div className="row">
+				<div className="col">
+				  <React.Fragment>
+					<Grid item xs={6} sm={6}>
+					  <Paper className={classes.paper} >{RailCard("Test-1","Natural","Alta","Acelga",10000000,600,60000)}</Paper>
+					</Grid>
+					<br/>
+					<Grid item xs={6} sm={20}>
+					  <Paper className={classes.paper} >{RailCard("Test-2","Natural","Alta","Acelga",10000000,600,60000)}</Paper>
+					</Grid>
+					<br/>
+					<Grid item xs={6}  sm={20}>
+					  <Paper className={classes.paper}>{RailCard("Test-3","Natural","Alta","Acelga",10000000,600,60000)}</Paper>
+					</Grid>
+				  </React.Fragment>
+		  		</div>
+			</div>
+		 </div>
     );
 }
