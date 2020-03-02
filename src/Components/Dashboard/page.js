f14794bb347eb9d04e8672ed4aa7cc3e5192af67
Alexander <heinzosesrunge@gmail.com>
2020-03-02 11:26:42 -0300

    arregla colores UI

--- src/Components/Dashboard/page.js
+++ src/Components/Dashboard/page.js
@@ -28,6 +28,7 @@ const useStyles = makeStyles((theme) => {
             flexGrow: 1,
             height:'100%',
             width:'100%',
+            padding: '10px',
         },
         paper: {
             textAlign: 'center',
